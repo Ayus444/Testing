6535933244fa88259253d1a016908ac1dad2229c
Ayush <112577569+Ayus444@users.noreply.github.com>
2025-02-24 01:34:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ bot = Client(
 )
 
 # Environment variables for API credentials
-API_ID = os.environ.get("API_ID", "21705536")
-API_HASH = os.environ.get("API_HASH", "c5bb241f6e3ecf33fe68a444e288de2d")
+API_ID = os.environ.get("API_ID", "")
+API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 
